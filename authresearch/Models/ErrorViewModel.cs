f97c6c7be101218b3439babdfff0357da4ae7d1c
devabituser <makoveychukvitaliy@gmail.com>
2018-03-27 14:54:20 +0300

    initial

---
+++ authresearch/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace authresearch.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
